3f9867133acd5cfb8a1bbaa33dd652d9a93af844
Dark Anon <paraglondhe123@gmail.com>
2020-08-16 16:58:00 +0530

    Updated stopwatch

--- Stopwatch.js
+++ Stopwatch.js
@@ -4,49 +4,25 @@ Sec = 00;
 Msec = 00;
 i = 00;
 var Correct;
-function updateMsecs() {//Modifies milliseconds
+function updatTime() {//Modifies milliseconds
     determiner = document.getElementById("STOP2").innerHTML;
     if (determiner == 'Stop') {
         Msec++;
     }
     if (Msec >= 1000) {
         Msec = 0;
-    }
-    let Timestr = Hrs + ":" + Min + ":" + Sec + ":" + Msec;
-    document.getElementById("Time").innerHTML = Timestr;
-}
-function updatesecs() {//modifies seconds
-    determiner = document.getElementById("STOP2").innerHTML;
-    if (determiner == 'Stop') {
         Sec++;
     }
-    if (Sec >= 60) {
-        Sec = 0;
+    if (Sec>=60) {
+        Sec=0;
+        Min++;  
     }
-    let Timestr = Hrs + ":" + Min + ":" + Sec + ":" + Msec;
-    document.getElementById("Time").innerHTML = Timestr;
-}
-function updatemins() {//Modifies minutes
-    determiner = document.getElementById("STOP2").innerHTML;
-    if (determiner == 'Stop') {
-        Min++;
-    }
-    if (Min >= 60) {
-        Min = 0;
-    }
-    let Timestr = Hrs + ":" + Min + ":" + Sec + ":" + Msec;
-    document.getElementById("Time").innerHTML = Timestr;
-}
-function updatehrs() {//Modifies hours
-    determiner = document.getElementById("STOP2").innerHTML;
-    if (determiner == 'Stop') {
+    if (Min>=60) {
+        Min=0;
         Hrs++;
     }
-    if (Hrs >= 24) {
-        Hrs = 0;
-    }
     let Timestr = Hrs + ":" + Min + ":" + Sec + ":" + Msec;
-    Correct = Timestr;
+    Correct=Timestr;
     document.getElementById("Time").innerHTML = Timestr;
 }
 function stoopper() {//Start or stop
@@ -64,12 +40,9 @@ function stoopper() {//Start or stop
 }
 
 //Main functioning area
-EventE = setInterval(stoopper, 1);
-EventA = setInterval(updateMsecs, 1);
-EventB = setInterval(updatesecs, 1000);
-EventC = setInterval(updatemins, 1000 * 62);
-EventD = setInterval(updatehrs, 1000 * 63 * 63);
-
+EventE = setInterval(stoopper, 10);
+EventA = setInterval(updatTime, 1);
+// stoopper();
 
 STOP3.addEventListener('click', function () {//Reset
     str = document.getElementById("BUTT1").innerHTML;
@@ -80,8 +53,8 @@ STOP3.addEventListener('click', function () {//Reset
     Sec = 00;
     Msec = 00;
     i = 0;
+    document.getElementById("STOP2").innerHTML ='Start';
 })
-prev = JUMBO.style.background
 STOP.addEventListener('click', function () {//Lapse
     str = (i + 1) + "→" + Hrs + ":" + Min + ":" + Sec + ":" + Msec;
     i++;
@@ -89,6 +62,7 @@ STOP.addEventListener('click', function () {//Lapse
     document.getElementById("BUTT1").innerHTML += str;
     document.getElementById("BUTT1").innerHTML += "<br><br>";
 })
+stoopper();
 
 
 
--- Stopwatch2.js
+++ Stopwatch2.js
@@ -10,22 +10,25 @@ option3.addEventListener('click', function () {
     let chngcol = document.getElementById("JUMBO");
     chngcol.style.background = "gold";
 })
+prev1 = JUMBO.style.background
 STOP.addEventListener('mouseover', function () {
     STOP.style.background = "rebeccapurple";
     flag = true;
 })
 STOP.addEventListener('mouseout', function () {
-    STOP.style.background = prev;
+    STOP.style.background = prev1;
 })
+prev2 = JUMBO.style.background
 STOP2.addEventListener('mouseover', function () {
     STOP2.style.background = "rebeccapurple";
 })
 STOP2.addEventListener('mouseout', function () {
-    STOP2.style.background = prev;
+    STOP2.style.background = prev2;
 })
+prev3 = JUMBO.style.background
 STOP3.addEventListener('mouseover', function () {
     STOP3.style.background = "rebeccapurple";
 })
 STOP3.addEventListener('mouseout', function () {
-    STOP3.style.background = prev;
+    STOP3.style.background = prev3;
 })
